52e1acf393d4e31d77ebb076a8ccdab78372a38e
Jon <jbrundagejr@gmail.com>
2021-07-28 15:27:01 -0400

    mailer working

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'lookfarcamera@gmail.com'
+  default from: ENV['email_user_name']
   layout 'mailer'
 end
--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,7 +2,7 @@ class MessageMailer < ApplicationMailer
 
   def new_message
     @email = params[:email]
-    mail(to: "jbrundagejr@gmail.com", subject: @email.subject)
+    mail(to: ENV['recipient_email'], subject: @email.subject)
   end
 
 end
