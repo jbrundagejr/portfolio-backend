2367756a90b8b38fb24f35c46daf8dce51589d64
jonbrundagejr <jbrundagejr@gmail.com>
2021-07-28 21:08:18 -0400

    air mailer

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,3 +1,3 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :name, :image, :blurb, :demo_link, :github_link, :made_with, :backend_repo
+  attributes :id, :name, :image, :blurb, :demo_link, :github_link, :made_with, :backend_repo, :link
 end
