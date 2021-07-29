ea4b28cea75e0bb216c44c3904c8456687e90011
Jon <jbrundagejr@gmail.com>
2021-07-28 20:27:07 -0400

    modal rework started

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210727163848) do
+ActiveRecord::Schema.define(version: 20210728201320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20210727163848) do
     t.datetime "updated_at", null: false
     t.string "backend_repo"
     t.string "made_with"
+    t.string "link"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@ Project.create(name: "Ice Cream Social",
                 blurb: "An app designed to connect ice cream enthusiasts both to each other through a direct messaging system, as well as
                   to new flavors and shops. Implements a React Redux frontend with a Ruby on Rails backend with JWT authorization for
                   user login and ActionCable for instant messaging.",
+                link: "",
                 demo_link: "https://vimeo.com/570414352",
                 github_link: "https://github.com/jbrundagejr/phase5-frontend",
                 backend_repo: "https://github.com/jbrundagejr/phase5-backend",
@@ -19,6 +20,7 @@ Project.create(name: "vinylboxd",
                   a Ruby on Rails backend, this site allows users to review records they've listened to as well as see what other
                   users have reviewed. Vinylboxd was also built with a JWT authorization for user login and a combination of 
                   Semantic React UI and vanilla CSS for styling.",
+                link: "https://vinylboxd.netlify.app/",
                 demo_link: "",
                 github_link: "https://github.com/jbrundagejr/phase-4-front-end",
                 backend_repo: "https://github.com/jbrundagejr/phase-4-backend",
@@ -29,6 +31,7 @@ Project.create(name: "Beer Snob",
                 blurb: "An app for reviewing beers and discovering new breweries. This was my first use of a React 
                 frontend after only a week and a half of study. Implements the useState hook for state management, as well as
                 Semantic React UI and vanilla CSS for styling.",
+                link: "https://beer-snob.netlify.app/",
                 demo_link: "",
                 github_link: "https://github.com/esellers25/beer-snob-frontend",
                 backend_repo: "",
