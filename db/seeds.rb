40d84e3e2d1c66496306cd87148b349fa75292a3
Jon <jbrundagejr@gmail.com>
2021-08-04 14:44:37 -0400

    image links changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Email.destroy_all
 puts "Creating Projects..."
 
 Project.create(name: "Ice Cream Social", 
-                image: "https://imgur.com/fe63t1z.png",
+                image: "https://imgur.com/pfEs9cC.jpg",
                 blurb: "An app designed to connect ice cream enthusiasts both to each other through a direct messaging system, as well as
                   to new flavors and shops. Implements a React Redux frontend with a Ruby on Rails backend with JWT authorization for
                   user login and ActionCable for instant messaging.",
@@ -15,7 +15,7 @@ Project.create(name: "Ice Cream Social",
                 made_with: "devicon-react-original-wordmark, devicon-redux-original, devicon-ruby-plain-wordmark, devicon-rails-plain-wordmark,
                 devicon-html5-plain-wordmark, devicon-css3-plain-wordmark")
 Project.create(name: "vinylboxd", 
-                image: "https://imgur.com/WKBlKTF.png",
+                image: "https://imgur.com/fwHqOx0.jpg",
                 blurb: "A recreation exercise of the great letterboxd site, but for vinyl records. Built with a React frontend and 
                   a Ruby on Rails backend, this site allows users to review records they've listened to as well as see what other
                   users have reviewed. Vinylboxd was also built with a JWT authorization for user login and a combination of 
@@ -27,7 +27,7 @@ Project.create(name: "vinylboxd",
                 made_with: "devicon-react-original-wordmark, devicon-ruby-plain-wordmark, devicon-rails-plain-wordmark,
                 devicon-html5-plain-wordmark, devicon-css3-plain-wordmark")
 Project.create(name: "Beer Snob", 
-                image: "https://imgur.com/I5cvGW8.png",
+                image: "https://imgur.com/JxHjOac.jpg",
                 blurb: "An app for reviewing beers and discovering new breweries. This was my first use of a React 
                 frontend after only a week and a half of study. Implements the useState hook for state management, as well as
                 Semantic React UI and vanilla CSS for styling.",
@@ -37,7 +37,7 @@ Project.create(name: "Beer Snob",
                 backend_repo: "",
                 made_with: "devicon-react-original-wordmark, devicon-html5-plain-wordmark, devicon-css3-plain-wordmark")
 Project.create(name: "This Porfolio", 
-                image: "https://imgur.com/SYSODKy.png",
+                image: "https://imgur.com/CPOmFem.jpg",
                 blurb: "I hoped to showcase a little of all my skills, with both a React frontend and Rails backend. I
                 wanted to present a little of my personality and tastes in design, with the goal of you seeing what your
                 own app could be.",
